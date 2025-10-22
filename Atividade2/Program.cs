00eff2324b8576969dc44461badfb409177d1f70
Henrique Reinaldo Jombra <444213@unoesc.lan>
2025-10-22 20:37:33 -0300

    first commit

---
+++ Atividade2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nomes = { "Maria", "João", "Ana", "Carlos", "Beatriz" };
+
+        BubbleSortDecrescente(nomes);
+
+        Console.WriteLine("Ordem Decrescente:");
+        foreach (string nome in nomes)
+        {
+            Console.Write(nome + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void BubbleSortDecrescente(string[] vetor)
+    {
+        int n = vetor.Length;
+        string temp;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                // compareTo retorna <0 se vetor[j] < vetor[j+1]
+                if (vetor[j].CompareTo(vetor[j + 1]) < 0)
+                {
+                    temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
